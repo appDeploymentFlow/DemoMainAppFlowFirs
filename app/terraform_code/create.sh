31f60370dbd5aab0558f343b680643ed347b3d83
vikas <vikasbhanuk@gmail.com>
2026-03-22 15:39:22 +0530

    modify terraform with bucket03

--- app/terraform_code/create.sh
+++ app/terraform_code/create.sh
@@ -2,7 +2,7 @@ cd app/terraform_code
 terraform fmt --recursive
 # # #terraform init -backend-config=state.tfvars
 # terraform init
-terraform init -backend-config=state.tfvars
+terraform init
 terraform plan
 sleep 5s
 terraform apply -auto-approve
--- app/terraform_code/providers.tf
+++ app/terraform_code/providers.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   region = "us-east-1"
 }
-# terraform {
-#   backend "s3" {
-#     bucket         = "your-terraform-state-bucket"
-#     key            = "path/to/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform-locks"  # Optional for locking
-#   }
-# }   
 terraform {
-  backend "s3" {}
-}
+  backend "s3" {
+    bucket         = "expense-bucket-01-705317504531-us-east-1-an"
+    key            = "expense/terraform.tfstate"
+    region         = "us-east-1"
+    #encrypt        = true
+    #dynamodb_table = "terraform-locks"  # Optional for locking
+  }
+}   
+# terraform {
+#   backend "s3" {}
+# }
