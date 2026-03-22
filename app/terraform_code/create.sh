1bae906e200fcf84892387c2a58588b10a92f8b6
vikas <vikasbhanuk@gmail.com>
2026-03-22 11:43:35 +0530

    modify workflow 11

--- app/terraform_code/create.sh
+++ app/terraform_code/create.sh
@@ -1,10 +1,10 @@
-# terraform fmt --recursive
+terraform fmt --recursive
 # #terraform init -backend-config=state.tfvars
-# terraform init
-# terraform plan
-# sleep 5s
-# terraform apply --auto-approve
-touch /home/ubuntu/test02
-pwd
-whoami
-terraform --version
+terraform init
+terraform plan
+sleep 5s
+terraform apply --auto-approve
+# touch /home/ubuntu/test02
+# pwd
+# whoami
+# terraform --version
