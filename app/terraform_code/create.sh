93add3f4a2c9acd122b97f776b760e52f40f837e
vikas <vikasbhanuk@gmail.com>
2026-03-22 13:17:12 +0530

    modify workflow 20

--- app/terraform_code/create.sh
+++ app/terraform_code/create.sh
@@ -1,10 +1,11 @@
-cd app/terraform_code
-terraform fmt --recursive
-# #terraform init -backend-config=state.tfvars
-terraform init
-terraform plan
-sleep 5s
-terraform apply -auto-approve
-# terraform -chdir=app/terraform_code init
-# terraform -chdir=app/terraform_code plan
-#terraform -chdir=app/terraform_code apply -auto-approve
+# cd app/terraform_code
+# terraform fmt --recursive
+# # #terraform init -backend-config=state.tfvars
+# terraform init
+# terraform plan
+# sleep 5s
+# terraform apply -auto-approve
+# # terraform -chdir=app/terraform_code init
+# # terraform -chdir=app/terraform_code plan
+# #terraform -chdir=app/terraform_code apply -auto-approve
+echo 'task is already done'
