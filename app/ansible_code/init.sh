7ebeebd6c76d4e0f72bb371d2f6c9a09e179e49e
vikas <vikasbhanuk@gmail.com>
2026-03-24 07:58:44 +0530

    update k8s install file04

--- app/ansible_code/init.sh
+++ app/ansible_code/init.sh
@@ -1,6 +1,6 @@
 #run docker role
 #ansible-pull -U https://vikaskumars1997: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@github.com/appDeploymentFlow/DemoMainAppFlowFirs.git -d /tmp/ansible_pull_cache -i localhost, -e "COMPONENT=docker" app/ansible_code/playbook.yml
-ansible-pull -U https://${1}:${2}@github.com/appDeploymentFlow/DemoMainAppFlowFirs.git -d /tmp/ansible_pull_cache -i localhost, -e "COMPONENT=${3}" app/ansible_code/playbook.yml
+#ansible-pull -U https://${1}:${2}@github.com/appDeploymentFlow/DemoMainAppFlowFirs.git -d /tmp/ansible_pull_cache -i localhost, -e "COMPONENT=${3}" app/ansible_code/playbook.yml
 # echo -e "accecc token for git is : ${1}, and my name is : ${2}"
 #${1} is the username of github
 #${2} is the github access token
@@ -11,7 +11,7 @@ ansible-pull -U https://${1}:${2}@github.com/appDeploymentFlow/DemoMainAppFlowFi
 
 ##run on worker01
 #get worker01 public ip
-# worker01=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=worker01" --query "Reservations[].Instances[].PublicIpAddress" --output text)
-# sshpass -p "${4}" ssh -o StrictHostKeyChecking=no ubuntu@$worker01 << 'EOF'
-# ansible-pull -U https://${1}:${2}@github.com/appDeploymentFlow/DemoMainAppFlowFirs.git -d /tmp/ansible_pull_cache -i localhost, -e "COMPONENT=${3}" app/ansible_code/playbook.yml
-# EOF
+worker01=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=worker01" --query "Reservations[].Instances[].PublicIpAddress" --output text)
+sshpass -p "${4}" ssh -o StrictHostKeyChecking=no ubuntu@$worker01 << 'EOF'
+ansible-pull -U https://${1}:${2}@github.com/appDeploymentFlow/DemoMainAppFlowFirs.git -d /tmp/ansible_pull_cache -i localhost, -e "COMPONENT=${3}" app/ansible_code/playbook.yml
+EOF
