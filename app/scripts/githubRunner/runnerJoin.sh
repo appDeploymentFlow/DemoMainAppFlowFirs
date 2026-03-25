1dc36b36ee0458eccf777b515ffb836ebd33861d
vikas <vikasbhanuk@gmail.com>
2026-03-25 18:57:25 +0530

    modify workflow 34

--- app/scripts/githubRunner/runnerJoin.sh
+++ app/scripts/githubRunner/runnerJoin.sh
@@ -10,17 +10,18 @@ sudo apt update
 #RUNNER_TOKEN=$(gh api --method POST /repos/appDeploymentFlow/DemoMainAppFlowFirs/actions/runners/registration-token -q .token)
 
 RUNNER_TOKEN=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${1}" -H "X-GitHub-Api-Version: 2026-03-10" https://api.github.com/repos/appDeploymentFlow/DemoMainAppFlowFirs/actions/runners/registration-token |jq -r '.token')
-echo "runner token is : ${RUNNER_TOKEN}"
+#echo "runner token is : ${RUNNER_TOKEN}"
 RUNNER_NAME="worker01"
-echo "runner name will be : ${RUNNER_NAME}"
+#echo "runner name will be : ${RUNNER_NAME}"
 REPO_URL="https://github.com/appDeploymentFlow/DemoMainAppFlowFirs"
 ##install github runner package
-mkdir actions-runner && cd actions-runner
+mkdir -p actions-runner && cd actions-runner
+#-p will ingnore error if directory is already present
 curl -o actions-runner-linux-x64-2.332.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.332.0/actions-runner-linux-x64-2.332.0.tar.gz
 echo "f2094522a6b9afeab07ffb586d1eb3f190b6457074282796c497ce7dce9e0f2a  actions-runner-linux-x64-2.332.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.332.0.tar.gz
 echo "current directory is : $(pwd)"
-./config.sh --unattended --replace --url $REPO_URL --token $RUNNER_TOKEN --name $RUNNER_NAME --labels linux,ubuntu,x64,$RUNNER_NAME --work _work
+./actions-runner/config.sh --unattended --replace --url $REPO_URL --token $RUNNER_TOKEN --name $RUNNER_NAME --labels linux,ubuntu,x64,$RUNNER_NAME --work _work
 sudo ./svc.sh install
 sudo systemctl start  actions.runner.appDeploymentFlow-DemoMainAppFlowFirs.$RUNNER_NAME.service
 EOF
