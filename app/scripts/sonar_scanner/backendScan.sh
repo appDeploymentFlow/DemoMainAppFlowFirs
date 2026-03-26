c7b33d2808fd0d632f54853fb572049063305073
vikas <vikasbhanuk@gmail.com>
2026-03-26 18:09:42 +0530

    scanner code modified01

--- app/scripts/sonar_scanner/backendScan.sh
+++ app/scripts/sonar_scanner/backendScan.sh
@@ -1,8 +1,10 @@
-sudo docker run -itd --name scanner_backend \
+sudo docker run -it --name scanner_backend \
 --network expense-network01 \
 -v app/backend:/usr/src \
+sonarsource/sonar-scanner-cli \
 -Dsonar.sources=. \
 -Dsonar.projectKey=${project_key} \
 -Dsonar.host.url=http://${WORKER01_PUBLIC_IP}:9000 \
 -Dsonar.token=${PROJECT_TOKEN} \
-sonarsource/sonar-scanner-cli
+-Dsonar.verbose=true \
+|tee app/scripts/sonar_scanner/backendScan.log
--- app/scripts/sonar_scanner/frontendScan.sh
+++ app/scripts/sonar_scanner/frontendScan.sh
@@ -1,8 +1,10 @@
-sudo docker run -itd --name scanner_frontend \
+sudo docker run -it --name scanner_frontend \
 --network expense-network01 \
+sonarsource/sonar-scanner-cli \
 -v app/frontend:/usr/src \
 -Dsonar.sources=. \
 -Dsonar.projectKey=${project_key} \
 -Dsonar.host.url=http://${WORKER01_PUBLIC_IP}:9000 \
 -Dsonar.token=${PROJECT_TOKEN} \
-sonarsource/sonar-scanner-cli 
+-Dsonar.verbose=true \
+|tee app/scripts/sonar_scanner/frontendScan.log
