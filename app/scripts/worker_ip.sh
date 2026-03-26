ab3f03b2f66adc00608c7c9e48dab737ff7fe628
vikas <vikasbhanuk@gmail.com>
2026-03-26 12:57:41 +0530

    modify workflow 47

--- app/scripts/worker_ip.sh
+++ app/scripts/worker_ip.sh
@@ -5,4 +5,4 @@
 # EOF
 echo export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 echo export WORKER01_PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
-echo "Worker01 public IP is: ${WORKER01_PUBLIC_IP}" >> /home/ubuntu/public.txt
+echo "Worker01 public IP is: ${WORKER01_PUBLIC_IP}"
