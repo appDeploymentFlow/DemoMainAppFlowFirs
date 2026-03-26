ea04b18ed9ef9d67f638fec0e06d640c57ebbdee
vikas <vikasbhanuk@gmail.com>
2026-03-26 13:24:40 +0530

    modify workflow 48

--- app/scripts/worker_ip.sh
+++ app/scripts/worker_ip.sh
@@ -1,8 +1,8 @@
-# cat << 'EOF' >> ~/.bashrc
-# # Fetch AWS IMDSv2 Token and Public IP
-# export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-# export WORKER01_PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
-# EOF
+cat << 'EOF' >> ~/.bashrc
+# Fetch AWS IMDSv2 Token and Public IP
+export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+export WORKER01_PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+EOF
 echo export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 echo export WORKER01_PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
-echo "Worker01 public IP is: ${WORKER01_PUBLIC_IP}"
+echo "Worker01 public IP is: ${WORKER01_PUBLIC_IP}" >> /home/ubuntu/public.txt
