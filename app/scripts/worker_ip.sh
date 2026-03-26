a49f61712546efc4e7846afa78f112dc431082e0
vikas <vikasbhanuk@gmail.com>
2026-03-26 12:02:23 +0530

    modify workflow 41

--- app/scripts/worker_ip.sh
+++ app/scripts/worker_ip.sh
@@ -1,7 +1,8 @@
-cat << 'EOF' >> ~/.bashrc
-# Fetch AWS IMDSv2 Token and Public IP
-export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-export WORKER01_PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
-EOF
-
+# cat << 'EOF' >> ~/.bashrc
+# # Fetch AWS IMDSv2 Token and Public IP
+# export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+# export WORKER01_PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+# EOF
+echo export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600") >> ~/.bashrc
+echo export WORKER01_PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4) >> ~/.bashrc
 source ~/.bashrc
