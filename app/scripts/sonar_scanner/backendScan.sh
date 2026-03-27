c7987235fc54855b9a2e5e83d84a0a45ab747000
vikas <vikasbhanuk@gmail.com>
2026-03-27 15:59:49 +0530

    scanner code modified12

--- app/scripts/sonar_scanner/backendScan.sh
+++ app/scripts/sonar_scanner/backendScan.sh
@@ -1,13 +1,25 @@
 echo "Backend project key is: ${backend_project_key}"
 echo "Backend project token is: ${BACKEND_PROJECT_TOKEN}"
 echo "URL of soanrqube server is: http://${WORKER01_PUBLIC_IP}:9000"
-sudo docker run --name scanner_backend \
+sudo docker run --rm --name scanner_backend \
 --network expense-network01 \
--v ./app/backend:/usr/src \
+-v ./app:/usr/src \
 sonarsource/sonar-scanner-cli \
 -Dsonar.projectKey=${backend_project_key} \
--Dsonar.sources=. \
+-Dsonar.sources=backend \
 -Dsonar.host.url=http://${WORKER01_PUBLIC_IP}:9000 \
 -Dsonar.token=${BACKEND_PROJECT_TOKEN} \
 -Dsonar.verbose=true \
 | tee app/scripts/sonar_scanner/backendScan.log
+
+#other 
+# sudo docker run --name scanner_backend \
+# --network expense-network01 \
+# -v ./app/backend:/usr/src \
+# sonarsource/sonar-scanner-cli \
+# -Dsonar.projectKey=${backend_project_key} \
+# -Dsonar.sources=. \
+# -Dsonar.host.url=http://${WORKER01_PUBLIC_IP}:9000 \
+# -Dsonar.token=${BACKEND_PROJECT_TOKEN} \
+# -Dsonar.verbose=true \
+# | tee app/scripts/sonar_scanner/backendScan.log
--- app/scripts/sonar_scanner/frontendScan.sh
+++ app/scripts/sonar_scanner/frontendScan.sh
@@ -1,13 +1,27 @@
+frontend_project_key=expense_frontend_01
+FRONTEND_PROJECT_TOKEN=squ_cb11ce9336f52e938519607eaf0971b1184b6c92
 echo "Frontend project key is: ${frontend_project_key}"
 echo "Frontend project token is: ${FRONTEND_PROJECT_TOKEN}"
 echo "URL of soanrqube server is: http://${WORKER01_PUBLIC_IP}:9000"
-sudo docker run --name scanner_frontend \
+sudo docker run --rm --name scanner_frontend \
 --network expense-network01 \
+-v ./app:/usr/src \
 sonarsource/sonar-scanner-cli \
--v ./app/frontend:/usr/src \
 -Dsonar.projectKey=${frontend_project_key} \
--Dsonar.sources=. \
+-Dsonar.sources=frontend \
 -Dsonar.host.url=http://${WORKER01_PUBLIC_IP}:9000 \
 -Dsonar.token=${FRONTEND_PROJECT_TOKEN} \
 -Dsonar.verbose=true \
 | tee app/scripts/sonar_scanner/frontendScan.log
+
+#other
+# sudo docker run --name scanner_frontend \
+# --network expense-network01 \
+# -v ./app/frontend:/usr/src \
+# sonarsource/sonar-scanner-cli \
+# -Dsonar.projectKey=${frontend_project_key} \
+# -Dsonar.sources=. \
+# -Dsonar.host.url=http://${WORKER01_PUBLIC_IP}:9000 \
+# -Dsonar.token=${FRONTEND_PROJECT_TOKEN} \
+# -Dsonar.verbose=true \
+# | tee app/scripts/sonar_scanner/frontendScan.log
